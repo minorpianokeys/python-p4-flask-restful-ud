deec656d7ccca014115d2893c64c71830e19524f
Michael Friedman <minorpianokeys@gmail.com>
2024-07-23 15:40:08 -0400

    complete

--- server/app.py
+++ server/app.py
@@ -79,6 +79,26 @@ class NewsletterByID(Resource):
         )
 
         return response
+    
+    def patch(self, id):
+        record = Newsletter.query.filter_by(id=id).first()
+        for attr in request.form:
+            setattr(record, attr, request.form.get(attr))
+
+        db.session.add(record)
+        db.session.commit()
+
+        record_dict = record.to_dict()
+        return make_response(record_dict, 200,)
+    
+    def delete(self, id):
+        record = Newsletter.query.filter_by(id=id).first()
+        db.session.delete(record)
+        db.session.commit()
+
+        response_dict = {"message": "record successfully deleted"}
+
+        return make_response(response_dict, 200,)
 
 api.add_resource(NewsletterByID, '/newsletters/<int:id>')
 
